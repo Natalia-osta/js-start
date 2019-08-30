87f20abb34dd90d41f20b2b9197f06f2e5eb059a
Natalia-osta <ostas.natali@gmail.com>
2019-08-30 16:46:54 +0000

    js

---
+++ js/index.js
@@ -0,0 +1,17 @@
+let message = 'Hello JavaScript!';
+alert(message);
+message = 'Nice to meet you !';
+alert(message);
+
+let userAge = parseInt(prompt('Enter your age '));
+alert('Your age is' + userAge);
+
+let nextAge = userAge + 1;
+alert('hext year you will be' + nextAge);
+
+ if (userAge >= 18 && userAge < 65 ) {
+     alert('you are abuld')
+ } else if (userAge < 65)
+ {
+     alert('you not ')
+ }
